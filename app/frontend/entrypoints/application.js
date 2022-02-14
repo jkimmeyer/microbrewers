b10043f72b852a0cbcd77215ead78eac479c7744
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-14 14:36:20 +0100

    Add routing without using vue templates for easy switching

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,34 +1,22 @@
 import { createApp } from 'vue'
+import App from '../app.vue'
+import * as VueRouter from 'vue-router'
 import Index from '../pages/index.vue'
+import About from '../pages/about.vue'
 
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+const routes = [
+  { path: '/', component: Index },
+  { path: '/about', component: About }
+]
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
 
 
-
-createApp(Index).mount('#app')
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+document.addEventListener('DOMContentLoaded', () => {
+  const app = createApp(App)
+  app.use(router)
+  app.mount('#app')
+})
--- app/frontend/pages/index.vue
+++ app/frontend/pages/index.vue
@@ -1,21 +1,12 @@
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div>
+    <p>Hello</p>
+  </div>
 </template>
 
 
 <script>
 export default {
   name: 'Index',
-  data() {
-    return {
-      count: 0
-    }
-  }
 }
 </script>
-
-<style scoped>
-button {
-  font-weight: bold;
-}
-</style>
