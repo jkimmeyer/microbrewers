1a6d5cf6e2c2853f97093badf9fa4b61cdf2999a
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-14 20:51:43 +0100

    Add routing (#3)
    * Add vue router
    
    * Add routing without using vue templates for easy switching
    
    * Remove obsolete references

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,34 +1,19 @@
 import { createApp } from 'vue';
+import * as VueRouter from 'vue-router';
+import App from '../App.vue';
 import Index from '../pages/index.vue';
 
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-// eslint-disable-next-line no-console
-console.log('Vite ⚡️ Rails');
+const routes = [
+  { path: '/', component: Index },
+];
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+});
 
-// eslint-disable-next-line no-console
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails');
-
-createApp(Index).mount('#app');
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+document.addEventListener('DOMContentLoaded', () => {
+  const app = createApp(App);
+  app.use(router);
+  app.mount('#app');
+});
