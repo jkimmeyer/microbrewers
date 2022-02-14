cc048734a531402ca39a051006bf08bd892a7acb
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-14 14:37:41 +0100

    Remove obsolete references

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import App from '../app.vue'
 import * as VueRouter from 'vue-router'
 import Index from '../pages/index.vue'
-import About from '../pages/about.vue'
 
 const routes = [
   { path: '/', component: Index },
-  { path: '/about', component: About }
 ]
 
 const router = VueRouter.createRouter({
