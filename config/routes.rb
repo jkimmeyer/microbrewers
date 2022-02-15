bbefb257c4e31487e015616f17ac88cda8042f84
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-15 08:21:09 +0100

    Run security checks and zeitwerk check on ci (#5)
    * Run security checks and zeitwerk check on ci
    
    * Fix ci
    
    * Add execution permission for ruby audit shell file
    
    * Fix zeitwerk

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "application#index"
-  get '/*path', to: 'application#index', inline: false
+  get "/*path", to: "application#index", inline: false
 end
