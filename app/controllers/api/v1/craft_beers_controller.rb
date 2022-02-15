fa688a0c48737062237207721d986d593c90b6f3
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-15 11:10:21 +0100

    Add create craft_beer route

--- app/controllers/api/v1/craft_beers_controller.rb
+++ app/controllers/api/v1/craft_beers_controller.rb
@@ -1,6 +1,31 @@
 module Api
   module V1
     class CraftBeersController < Api::ApplicationController
+      def create
+        @craft_beer = CraftBeer.new(craft_beer_params)
+
+        if @craft_beer.save
+          render json: @craft_beer
+        else
+          render json: { errors: @craft_beer.errors }
+        end
+      end
+
+      private
+
+      def craft_beer_params
+        params.require(:craft_beer).permit(%i[
+                                             name
+                                             description
+                                             international_bitterness_unit
+                                             alcohol_volume
+                                             price
+                                             flavor
+                                             color
+                                             hop
+                                             craft_beer_type_id
+                                           ])
+      end
     end
   end
 end
