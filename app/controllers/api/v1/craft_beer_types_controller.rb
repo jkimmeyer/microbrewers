8e46b56f83183439b512243a394378e6880e4817
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-15 10:49:42 +0100

    Add default controllers and route configuration

---
+++ app/controllers/api/v1/craft_beer_types_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class CraftBeerTypesController < Api::ApplicationController
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  namespace :api do
+    namespace :v1 do
+      resources :craft_beers
+      resources :craft_beer_types
+    end
+  end
+
   root "application#index"
   get "/*path", to: "application#index", inline: false
 end
