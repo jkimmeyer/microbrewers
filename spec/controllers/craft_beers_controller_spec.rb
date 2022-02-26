a8caa398dfbb394c084809ee9e919c9abe54b7e6
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-27 00:34:07 +0100

    Add storage spec and refactor existing specs to use a subject

--- spec/controllers/craft_beers_controller_spec.rb
+++ spec/controllers/craft_beers_controller_spec.rb
@@ -1,56 +1,72 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::CraftBeersController do
-  describe "POST create" do
+  describe "POST /api/v1/craft_beers/create" do
+
     context "with valid attributes" do
+      subject { post :create, params: craft_beer }
+
       let(:craft_beer_type) { create :craft_beer_type }
       let(:craft_beer) do
-        { craft_beer: { name: "Holunder-Bier", craft_beer_type_id: craft_beer_type.id } }
+        { craft_beer: { name: "Holunder-Bier", craft_beer_type_id: craft_beer_type.id, craft_beer_image: craft_beer_image } }
       end
 
-      it "returns a json object" do
-        post :create, params: craft_beer
+      let(:craft_beer_image) { fixture_file_upload("orange.png") }
+
+      it "returns a JSON response" do
+        subject
         expect(response.content_type).to eq "application/json; charset=utf-8"
       end
 
       it "creates a craft beer" do
-        expect { post :create, params: craft_beer }.to change { CraftBeer.count }.from(0).to(1)
+        expect { subject }.to change { CraftBeer.count }.from(0).to(1)
+      end
+
+      it "creates a blob " do
+        expect { subject }.to change { ActiveStorage::Blob.count }.from(0).to(1)
       end
     end
 
     context "with invalid attributes" do
+      subject { post :create, params: invalid_craft_beer }
+
       let(:invalid_craft_beer) do
         { craft_beer: { name: "Holunder-Bier" } }
       end
 
       it "returns errors" do
-        post :create, params: invalid_craft_beer
+        subject
         expect(response.body).to eq({ errors: { craft_beer_type: ["must exist"] } }.to_json)
       end
 
       it "creates no beer" do
-        expect { post :create, params: invalid_craft_beer }.not_to(change { CraftBeer.count })
+        expect { subject }.not_to(change { CraftBeer.count })
       end
 
       it "returns json" do
-        post :create, params: invalid_craft_beer
+        subject
 
         expect(response.content_type).to eq "application/json; charset=utf-8"
+      end
+
+      it "does not create a blob" do
+        expect { subject }.not_to change { ActiveStorage::Blob.count }.from(0)
       end
     end
   end
 
-  describe "GET index" do
+  describe "GET /api/v1/craft_beers" do
+    subject { get :index}
     let!(:craft_beers) { create_list(:craft_beer, 2) }
 
     it "returns a json array" do
-      get :index
+      subject
       expect(response.content_type).to eq "application/json; charset=utf-8"
       expect(JSON.parse(response.body)).to be_an Array
     end
 
     it "returns two craft beers" do
-      get :index
+      subject
       expect(JSON.parse(response.body).size).to eq 2
       expect(response.body).to eq craft_beers.to_json
     end
