1dccbbd2ba72cfcd99c367ef143db47b4aa2cca6
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-27 16:18:55 +0100

    Add styleguide (#13)
    * Add initial styleguide
    
    * Add styleguide and apply to items

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     './app/frontend/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      saffron: '#F8C630',
+      limeGreen: '#6BD425',
+      oxfordBlue: '#101935',
+      lavenderWeb: '#EEEEFF',
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+    fontFamily: {
+      sans: ['Darker Grotesque', 'sans-serif'],
+      serif: ['Supermercado One', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
