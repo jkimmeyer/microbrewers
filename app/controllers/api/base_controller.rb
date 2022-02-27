c22390bc2dce19a803b9418db18cb5c6c077b417
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-27 18:17:59 +0100

    Add base authentication on api

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,7 @@
 module Api
   class BaseController < ActionController::Base
     include DeviseTokenAuth::Concerns::SetUserByToken
+    before_action :authenticate_user!
     protect_from_forgery with: :null_session
   end
 end
