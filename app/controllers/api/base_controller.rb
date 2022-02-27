d298733230a25e9e88c7124833685faea32fad3b
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-27 18:05:32 +0100

    Fix base controller name

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class ApplicationController < ActionController::Base
+  class BaseController < ActionController::Base
     include DeviseTokenAuth::Concerns::SetUserByToken
     protect_from_forgery with: :null_session
   end
