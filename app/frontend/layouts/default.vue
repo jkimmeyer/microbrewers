b78bdacb4cf5b084ce1dd34841ada8841eb6a307
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-27 19:00:52 +0100

    Add a layout component (#16)
    * Add a layout component
    
    * Add basic navigation bar
    
    * Add basic site spec

---
+++ app/frontend/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-lavenderWeb h-screen">
+    <Navigation />
+    <slot />
+  </div>
+</template>
+<script>
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+  name: 'LayoutDefault',
+  components: { Navigation },
+};
+</script>
--- app/frontend/locales/de.js
+++ app/frontend/locales/de.js
@@ -1,9 +1,12 @@
 import brewer from '@/locales/brewer/brewer.de.json';
 import craftBeer from '@/locales/craftBeer/craftBeer.de.json';
+import navigation from '@/locales/navigation.de.json';
 
 export const messages = {
   de: {
+    microbrewers: 'Microbrewers',
     brewer,
     craftBeer,
+    navigation,
   },
 };
