1cbf3ce0257c49919f9f4cc352476c35d4f164d5
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-27 15:04:20 +0100

    Add initial styleguide

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     './app/frontend/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      saffron: '#F8C630',
+      'lime-green': '#6BD425',
+      'oxford-blue': '#101935',
+      'lavender-web': '#EEEEFF',
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+    fontFamily: {
+      sans: ['Darker Grotesque', 'sans-serif'],
+      serif: ['Supermercado One', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
