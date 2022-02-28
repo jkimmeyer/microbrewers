7865b8f6339522e4b896922dcf999125c0e10fff
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-02-28 17:06:40 +0100

    Extract route config to routes.js config file

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -4,19 +4,11 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import { createI18n } from 'vue-i18n';
 import { messages } from '@/locales/de';
+import { routes } from '@/config/routes';
 
 import App from '@/App.vue';
-import Index from '@/pages/index.vue';
-import CraftBeersNew from '@/pages/craft_beers/new.vue';
-import CraftBeersIndex from '@/pages/craft_beers/index.vue';
 
 import '@/assets/index.css';
-
-const routes = [
-  { path: '/', component: Index },
-  { path: '/craft_beers/new/', component: CraftBeersNew },
-  { path: '/craft_beers', component: CraftBeersIndex },
-];
 
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
