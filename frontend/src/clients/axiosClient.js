a54b4f100ed2b380754aa3495171236a564e8008
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-03-01 15:59:22 +0100

    Define vue/vite app

--- frontend/src/clients/axiosClient.js
+++ frontend/src/clients/axiosClient.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = '/api/v1';
+const VUE_API_BASE_HOST = 'http://localhost:3000';
+const baseURL = `${VUE_API_BASE_HOST}/api/v1`;
 
 export default axios.create({
   baseURL,
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 import path from 'path';
 
-export default defineConfig({
+export default () => defineConfig({
   plugins: [
     vue(),
   ],
+  define: {
+    'process.env': process.env,
+  },
   resolve: {
     alias: [{
       find: '@', replacement: path.resolve(__dirname, './src'),
