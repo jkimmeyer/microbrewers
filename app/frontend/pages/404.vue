f96a696d26078bd8f7eac66bf25704ca49ca2604
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-03-01 14:26:56 +0100

    Add error pages (#18)
    * Extract route config to routes.js config file
    
    * Add error routes and error component

---
+++ app/frontend/pages/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <ErrorHint
+    :error-code="404"
+    :error-title="$t('navigation.notFound.title')"
+    :error-description="$t('navigation.notFound.description')"
+    error-route="/"
+    :error-route-title="$t('navigation.root')"
+  />
+</template>
+
+<script>
+import ErrorHint from '@/components/ErrorHint.vue';
+
+export default {
+  name: 'NotFound',
+  components: {
+    ErrorHint,
+  },
+};
+</script>
