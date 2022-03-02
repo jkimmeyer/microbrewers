1caf91fa8d310c65c71a6ce9fed26381488bc1ee
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-03-02 15:27:53 +0100

    Ignore cypress videos and screenshots

--- frontend/cypress/integration/Homepage.js
+++ frontend/cypress/integration/Homepage.js
@@ -1,5 +1,5 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('http://localhost:4173'); // change URL to match your dev URL
+    cy.visit('http://localhost:4173');
   });
 });
