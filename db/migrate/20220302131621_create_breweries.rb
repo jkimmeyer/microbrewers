cfa9446800f9c7fbb2cc775f69fc233f19183347
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-03-02 14:17:02 +0100

    Add brewery model

---
+++ db/migrate/20220302131621_create_breweries.rb
@@ -0,0 +1,8 @@
+class CreateBreweries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :breweries do |t|
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/breweries.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :brewery do
+    
+  end
+end
