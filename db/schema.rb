48ad325b02791e3fb0b4f38779e6e3bf8f33cef9
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-03-22 18:40:46 +0100

    Add crowd brew

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_144809) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_22_171949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_144809) do
     t.json "flavors_data"
     t.index ["craft_beer_type_id"], name: "index_craft_beers_on_craft_beer_type_id"
     t.index ["name"], name: "index_craft_beers_on_name", unique: true
+  end
+
+  create_table "crowd_brews", force: :cascade do |t|
+    t.date "producing_at"
+    t.date "crowd_brew_until"
+    t.date "crowd_brew_from"
+    t.date "estimated_delivery_date"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
