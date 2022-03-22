561ab94be8e0f35c4025a9e96a0a3d3d68fc3a4c
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-03-22 12:21:34 +0100

    Fix rubocop

--- app/controllers/overrides/registrations_controller.rb
+++ app/controllers/overrides/registrations_controller.rb
@@ -8,7 +8,7 @@ module Overrides
       devise_parameter_sanitizer.permit(:sign_up, keys: [
         :account_type,
         { account_attributes:
-          [:name, :description, :ust_id, :first_name, :last_name, :phone,  { address:
+          [:name, :description, :ust_id, :first_name, :last_name, :phone, { address:
              %i[street house_number postal_code city country] }] },
       ])
     end
